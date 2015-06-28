0921a868d551e9c86333f9be9f7c295cca210011
Shirley Miao <shirleycmiao@gmail.com>
2015-06-27 21:15:47 -0700

    Big project dump with wizard wands

---
+++ Assets/Scripts/Magic.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+var magicBullet : Transform;
+
+function Start () {
+
+}
+
+// Should be a plane instead of cylindrical input
+function Update () {
+	if (Input.GetKeyUp("e")) {
+		var magicFire = Instantiate (magicBullet, gameObject.Find("magicSpawnPoint").transform.position, Quaternion.Euler(90, 0, 0));
+		var rb = magicFire.GetComponent.<Rigidbody>();
+		rb.AddForce(transform.up * 2000);
+	}
+}
+
+/*function ThrowMagic() {
+	var baseTransform = this.GetComponentInChildren<Transform>(true)[0];
+	var baseRotation = baseTransform.rotation;
+	
+}*/
